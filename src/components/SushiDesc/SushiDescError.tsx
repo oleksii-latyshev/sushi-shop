bde793d8c64e8fa5bc7701cbd1c7e5b899693a36
Alexey Latyshev <latyshevskiymail@gmail.com>
2023-04-21 12:35:00 +0300

    style: error page for sushi

--- src/components/SushiDesc/SushiDescError.tsx
+++ src/components/SushiDesc/SushiDescError.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import emptyBox from '../../assets/img/emptyBox.png';
 import styles from './SushiDesc.module.scss';
 
 const SushiDescError = () => {
-  return <div className={styles.errorWrapper}>Выбранная суши не существует</div>;
+  return (
+    <div className={styles.errorWrapper}>
+      <img src={emptyBox} alt='empty' />
+      <h2>Выбранная суши не существует 😥</h2>
+      <Link to='/'>
+        <button>вернуться назад</button>
+      </Link>
+    </div>
+  );
 };
 
 export default SushiDescError;
